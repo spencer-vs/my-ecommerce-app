eeb88205dd096208bf1fb362ffa19c1de2162b9c
spencer-vs <isaacharu17@gmail.com>
2025-11-25 06:15:46 +0100

    database fixes

--- SHOPITT/settings.py
+++ SHOPITT/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 from datetime import timedelta
 
-
+import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -118,10 +118,10 @@ WSGI_APPLICATION = 'SHOPITT.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
 }
 
 
