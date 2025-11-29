1eb19a18de7921a69887608113b78bd4a936dcbd
spencer-vs <isaacharu17@gmail.com>
2025-11-29 05:46:06 +0100

    version 0.8

--- SHOPITT/settings.py
+++ SHOPITT/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_3h7mgs1o1w6=0%(czwg0s2&@21%4f)j($-z_)!i*ltv%(!6m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'my-ecommerce-app-z1s7.onrender.com' ]
 
